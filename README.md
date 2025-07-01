9b3fec48ade1a636b5715aecb099a4c30c800fe5
HadiMaalikk <Hadimaliyakkal6@gmail.com>
2025-07-01 20:42:38 +0530

    Made the base

--- README.md
+++
@@ -1,5 +0,0 @@
-# Portfolio
-My portfolio!
-
-
-Check out my LinkedIn: https://www.linkedin.com/in/hadi-m/
---
+++ port/src/App.tsx
@@ -0,0 +1,9 @@
+const  App =()=> {
+  return (
+    <>
+    <p className="text-red-500 text-center text-[40px]">Hi my name is hadi</p>
+    </>
+  )
+}
+
+export default App
